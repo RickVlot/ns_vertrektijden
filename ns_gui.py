083a109a6fdef31a229edadda9914ef2155b4076
Rick Vlot <rick.vlot@student.hu.nl>
2017-01-13 20:40:34 +0100

    - Docstrings toegevoegd - Try/except statements toegevoegd

--- ns_gui.py
+++ ns_gui.py
@@ -1,17 +1,19 @@
 from tkinter import *
 import requests
 import xmltodict
-import tkinter.messagebox
-import tkinter.font
 
 
 
-root = Tk()
-root.config(bg="#FFCC18")  # Opmaak van GUI, Gijs Willemse 3-11-2016
-root.wm_title("NS Actuele Vertrektijden")
-root.iconbitmap(r'C:\Users\Gebruiker\Downloads\nsiconbmp.bmp')
+def api(station):
+    """Return train information from the NS
 
-def api(station):  # Gemaakt door Rick Vlot, modified by Rick Vlot 3-11-2016
+    Return from API:
+
+        Eindbestemming
+        Vertrektijd
+        Vertrekspoor
+        Treinsoort
+    """
     station = e.get()
     text.config(state=NORMAL)
     text.delete('1.0', 'end')
@@ -21,20 +23,29 @@ def api(station):  # Gemaakt door Rick Vlot, modified by Rick Vlot 3-11-2016
 
     vertrekXML = xmltodict.parse(response.text)
 
-    text.insert('1.0', 'Dit zijn de vertrekkende treinen:' + '\n')
-    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-        eindbestemming = vertrek['EindBestemming']
+# Schrijf een try-exept om fouten te kunnen voorkomen
+    try:
+        for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
 
-        vertrektijd = vertrek['VertrekTijd']    # 2016-09-27T18:36:00+0200
-        vertrektijd = vertrektijd[11:16]        # 18:36
-        vertrekspoor = str(vertrek['VertrekSpoor']['#text'])
-        treintype = vertrek['TreinSoort']
+            eindbestemming = vertrek['EindBestemming']
 
-        text.insert('end', 'Om '+vertrektijd+' vertrekt een '+treintype+' naar '+eindbestemming+' vanaf spoor '+vertrekspoor + '\n')
-    text.config(state=DISABLED)
+            vertrektijd = vertrek['VertrekTijd']
+            vertrektijd = vertrektijd[11:16]
+            vertrekspoor = str(vertrek['VertrekSpoor']['#text'])
+            treintype = vertrek['TreinSoort']
 
+            text.insert('end', 'Om '+vertrektijd+' vertrekt een '+treintype+' naar '+eindbestemming+' vanaf spoor '+vertrekspoor + '\n')
+        text.config(state=DISABLED)
+    except KeyError:
+        text.insert('1.0', 'Dit station bestaat niet.' + '\n')
+        text.insert('1.0', 'Voer a.u.b een bestaand station in.' + '\n')
 
+# Maak met behulp van Tkinter een interface-scherm
+root = Tk()
+root.config(bg="#FFCC18")
+root.wm_title("NS Actuele Vertrektijden")
 
+# Maak een textbox aan waarin men een station in kan voeren
 label1 = Label(text="Vul hier het station in waarvan u de reisinformatie wilt opvragen: ", bg="#FFCC18")
 label1.pack(side=TOP)
 
@@ -42,15 +53,15 @@ e = Entry(root, font="Arial", width=37)
 e.bind("<Return>", api)
 e.pack()
 
-photo = PhotoImage(file="nspoep.png")  # NS Logo toegevoegd Gijs Willemse 3-11-2016
-
-label = Label(image=photo)  # NS Plaatje toegevoegd Doeke Roos, Gijs Willemse 3-11-2016
+# Voeg een plaatje van het NS-logo toe
+photo = PhotoImage(file="nspoep.png")
+label = Label(image=photo)
 label.pack(side=BOTTOM)
 
+# Codeer hoe de tekst eruit moet zien die verschijnt op het beeldscherm
 text = Text(root, width=70, height=25, bg="#FFCC18")
 text.config(state=DISABLED)
 text.config(font=("Arial", 12,))
 text.pack()
-
 
 root.mainloop()
